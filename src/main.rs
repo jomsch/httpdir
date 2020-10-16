b324b7d7ca7331f0bc6dd14f9d7125f888f2f314
jomsch <jomsch@jomsch.de>
2020-10-16 20:53:13 +0200

    add hello world web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use async_std::prelude::*;
+
+#[async_std::main]
+async fn main() -> tide::Result<()> {
+    let mut app = tide::new();
+    app.at("/").get(|r| async { Ok("Hello World") });
+    app.listen("0.0.0.0:8000").await?;
+
+    Ok(())
 }
