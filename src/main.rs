23cab4dad1e72bc78b08aeaedabe4f8ad10ee390
jomsch <jomsch@jomsch.de>
2020-10-18 10:38:27 +0200

    add logging

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-use async_std::prelude::*;
+extern crate env_logger;
+extern crate log;
+
+const PORT: u32 = 8888;
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
+    env_logger::init();
+
     let mut app = tide::new();
-    app.at("/").get(|r| async { Ok("Hello World") });
-    app.listen("0.0.0.0:8000").await?;
+    app.at("/").get(|_| async { Ok("Hello World") });
+
+    app.listen(format!("http://0.0.0.0:{}", PORT)).await?;
 
     Ok(())
 }
