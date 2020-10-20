609bd4d044fa038b98161501eca71a647641e67d
jomsch <jomsch@jomsch.de>
2020-10-20 20:16:41 +0200

    add directory entries as html with hrefs

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use async_std::fs;
 use async_std::prelude::*;
 
 const PORT: u32 = 8888;
-const SERVE_DIR_PATH: &str = "./";
+const SERVE_DIR_PATH: &str = ".";
 const SERVE_DIR_ROUTE: &str = "/httpdir";
 
 #[async_std::main]
@@ -24,17 +24,36 @@ async fn main() -> tide::Result<()> {
 }
 
 async fn serve_dir_at_route(req: tide::Request<()>) -> tide::Result<tide::Response> {
-    let url_path = &req.url().path()[1..];
-    let file_path = format!("{}{}", SERVE_DIR_PATH, url_path);
-    let file_path_metadata = fs::metadata(&file_path).await?;
-    if !file_path_metadata.is_dir() {
-        return Ok(tide::Response::builder(404).build());
+    log::info!("URL PATH: {}", &req.url());
+    let url_path = &req.url().path();
+    let dir_path = format!("{}{}", SERVE_DIR_PATH, url_path);
+
+    let mut response = match fs::metadata(&dir_path).await {
+        Ok(md) if md.is_dir() => tide::Response::builder(200)
+            .content_type(tide::http::mime::HTML)
+            .build(),
+        _ => return Ok(tide::Response::new(404)),
+    };
+
+    let mut entries = fs::read_dir(&dir_path).await?;
+    let mut entries_html = String::new();
+
+    while let Some(res) = entries.next().await {
+        let entry = res?;
+        let file_name: String = entry.file_name().to_string_lossy().into();
+        let metadata = entry.metadata().await?;
+        let src = if metadata.is_dir() {
+            format!("/{}", file_name)
+        } else {
+            format!("{}{}{}", SERVE_DIR_ROUTE, url_path, file_name)
+        };
+
+        entries_html.push_str(&format!("<li><a href={}>{}</a></li>", src, file_name));
     }
 
-    let response = tide::Response::builder(200)
-        .body(file_path)
-        .content_type(tide::http::mime::HTML)
-        .build();
+    let body = format!("<ul>{}</ul>", entries_html);
+
+    response.set_body(body);
 
     Ok(response)
 }
